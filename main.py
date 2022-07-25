9236405bbed491db33996e3070834229d11cb47b
daraya-reservo <104777673+daraya-reservo@users.noreply.github.com>
2022-07-25 15:02:26 -0400

    bot

--- main.py
+++ main.py
@@ -13,12 +13,19 @@ from slackeventsapi import SlackEventAdapter
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 locale.setlocale(locale.LC_TIME, '')
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+# slack_token = os.environ['SLACK_TOKEN']
+slack_token = 'xoxb-3843377153700-3834351130870-M0BORa58AgcBx6spzbGWMMTD'
+client = slack.WebClient(token=slack_token)
 xlsx_file = 'app/Dailys_ Equipo TI Reservo_20220720.xlsx'
 app = Flask(__name__)
+# signing_secret = os.environ['SIGNING_SECRET']
+signing_secret = '6d78dcf575bd54288e0c074ac384ad35'
 slack_events_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
+    signing_secret, '/slack/events', app)
 BOT_ID = client.api_call('auth.test')['user_id']
+@app.route('/')
+def home():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
 
 today = datetime.now()
 today_str = today.isoformat().split('T')[0]
@@ -29,17 +36,12 @@ df_dailies = pd.read_excel(xlsx_file, sheet_name='Table 2')
 
 @slack_events_adapter.on('message')
 def message(payload):
-    # print(payload)
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
     if BOT_ID != user_id:
         client.chat_postMessage(channel=channel_id, text=text)
-
-@app.route('/')
-def home():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
 
 @app.route('/daily', methods=['POST'])
 def daily():
